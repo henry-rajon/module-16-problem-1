a86d6dabf83c56e3a51f18f516499cec8f19b523
Raj_Dash <rajdash3472@gmail.com>
2022-07-31 21:59:32 +0600

    first commit

---
+++ module-16.js
@@ -0,0 +1,46 @@
+// Problem-Solve-1
+
+var momGivesMomey = 1000;
+var applePrice = 300;
+var orangePrice = 400;
+
+var totalMoney = (applePrice + orangePrice);
+// console.log(totalMoney);
+
+var shopkeeperReturn = (momGivesMomey - totalMoney);
+// console.log(shopkeeperReturn);
+
+
+// Problem-Solve-2
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = (mathematics + biology + chemistry + physics + bangla);
+// console.log(totalMarks);
+
+var averageMarks = (totalMarks / 5);
+// console.log(averageMarks);
+
+var num = averageMarks.toFixed(2);
+
+// console.log(num);
+
+var changeString = parseFloat(num);
+// console.log(changeString);
+
+
+// Problem-Solve-3
+var firstVariable = "I am going to be";
+var secondVariable = "an awesome web developer";
+var combineVariable = firstVariable + " " + secondVariable;
+// console.log(combineVariable);
+
+
+// Problem-Solve-4
+var price = 119;
+var divided = (price % 5);
+console.log(divided);
